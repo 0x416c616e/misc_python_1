758a8d4d34670c11e895b66bf3b836617970de39
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-03 19:50:46 -0600

    added new python crash course directory

---
+++ PythonCrashCourse/chapter2.py
@@ -0,0 +1,11 @@
+#2-1 simple message as described in book
+message = "this is a message just for you!"
+print(message)
+#2-2 change value and then print
+#this is too easy, why am I wasting my time on this?
+message = "this is a new message"
+print(message)
+string1 = 'single quotes'
+string2 = "double quotes"
+print("in python, you can use " + string1 + " or " + string2, end="")
+print(" and the last print() had no \nnewline afterwards")
