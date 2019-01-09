9cab0498ef90ea6e2b907d37b45928f833258664
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-09 01:00:09 -0600

    more chapter 4 stuff

--- PythonCrashCourse/chapter4.py
+++ PythonCrashCourse/chapter4.py
@@ -48,4 +48,9 @@ numbers = list(range(1,6))  # 1,2,3,4,5
 even_numbers = list(range(2,11,2)) # 2,4,6,8,10
 print(even_numbers)
 
+squares = []
+for value in range(1,11):
+    square = value**2
+    squares.append(square)
+print(squares)
 
