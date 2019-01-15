a958d5699455dd12521d8d9afb197e856c451328
Alan <596a75b727b37a43b8c5b5f142329ab3@protonmail.com>
2019-01-15 03:53:42 -0600

    did some chapter 7 stuff

---
+++ PythonCrashCourse/chapter7.py
@@ -0,0 +1,17 @@
+print("Chapter 7: user inputs and while loops")
+# the input() function
+# I already used this in previous chapter files
+# and just previously in python in general
+# nothing new here
+
+message = input("enter your message and I will print it back to you: ")
+print(message)
+
+# typecasting to int
+some_number = input('enter a number: ')
+some_other_number = int(some_number) + 1
+print(some_other_number)
+
+# some other useful but unrelated stuff from a udemy coures include:
+# split() and join()
+
