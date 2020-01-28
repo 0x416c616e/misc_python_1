ae0f8ddf6bd92c80406d68e86b5656aa3db1c1ce
0x416c616e <29168822+0x416c616e@users.noreply.github.com>
2020-01-28 15:42:26 -0600

    added vowel counter exercise

---
+++ count_vowels/vowels.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import sys
+
+def count_vowels(str_to_check):
+    num = 0
+    vowels = "a e i o u".split(" ")
+    for char in str_to_check:
+        if char in vowels:
+            num += 1
+    return num
+
+def main():
+    if (len(sys.argv) == 1):
+        print("Error: lacking CLI args")
+        print("Use it like this: count_vowels.py hello world ok")
+        sys.exit(1)
+    else:
+        total = 0
+        print(sys.argv[1])
+        for i in range(1, len(sys.argv)):
+            total += count_vowels(sys.argv[i])
+    print(str(total) + " vowel", end="")
+    if (total > 1):
+        print("s")
+    else:
+        print("")
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print("\nQuitting. Goodbye.")
+        sys.exit()
